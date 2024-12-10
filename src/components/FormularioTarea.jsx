6e53883099c773fcc189aa4891c39f09f6362de4
Emilse Arias <emilsearias@gmail.com>
2024-12-10 11:39:40 -0300

    agregamos la validacion de campo requerido en el input para cargar una tarea

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -2,13 +2,23 @@ import { Button } from "react-bootstrap";
 import { Form } from "react-bootstrap";
 import { PlusCircle } from "react-bootstrap-icons";
 import ListaTareas from './ListaTareas'
+import { useForm } from "react-hook-form";
 
 const FormularioTarea = () => {
+  const {register, handleSubmit,  formState: { errors }} = useForm();
+
+  const posteriorValidacion = (data)=>{
+    console.log(data)
+  }
+
     return (
         <section className="mb-4">
-          <Form className="container d-flex">
+          <Form onSubmit={handleSubmit(posteriorValidacion)} className="container d-flex">
             <Form.Group controlId="formTarea" className="w-100">
-              <Form.Control type="text" placeholder="Ingrese una tarea" />
+              <Form.Control type="text" placeholder="Ingrese una tarea" {...register("tarea",{
+                required: 'La tarea es un dato obligatorio'
+              })}/>
+              <Form.Text className="text-danger">{errors.tarea?.message}</Form.Text>
             </Form.Group>
             <Button variant="primary" type="submit">
               <PlusCircle></PlusCircle>
